ac0a26f5bd7e02580007a19062f1278c3bfe0520
TapamitaMondal07 <tapomita.tae007@gmail.com>
2024-09-16 22:45:07 +0530

    connect to the database

--- server/index.js
+++ server/index.js
@@ -1,4 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO)
+.then(()=>{
+  console.log('Mongo Db is connected')
+})
+.catch(()=>{
+  console.log('Error')
+})
 
 const app = express();
 
