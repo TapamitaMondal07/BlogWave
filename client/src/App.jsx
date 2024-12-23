2c77439465dfb4c85e701b64edcc5c5cb4ae5478
TapamitaMondal07 <tapomita.tae007@gmail.com>
2024-12-24 04:23:30 +0530

    complete search page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,6 +15,7 @@ import CreatePost from './pages/CreatePost';
 import UpdatePost from './pages/UpdatePost';
 import PostPage from './pages/PostPage';
 import ScrollToTop from './components/ScrollToTop';
+import Search from './pages/Search';
 
 import { TextInput } from 'flowbite-react'
 
@@ -29,6 +30,7 @@ function App() {
         <Route path="/about" element={<About/>}/>
         <Route path="/sign-in" element={<Signin/>}/>
         <Route path="/sign-up" element={<Signup/>}/>
+        <Route path='/search' element={<Search />} />
         <Route element={<PrivateRoute/>}>
           <Route path="/dashboard" element={<Dashboard/>}/>
         </Route>
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,16 +1,28 @@
-import { Avatar, Button, Dropdown, Navbar } from 'flowbite-react';
-import { Link, useLocation } from 'react-router-dom';
+import { Avatar, Button, Dropdown, Navbar, TextInput } from 'flowbite-react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { AiOutlineSearch } from 'react-icons/ai';
 import { FaMoon, FaSun } from 'react-icons/fa';
 import { useSelector, useDispatch } from 'react-redux';
 import { toggleTheme } from '../redux/theme/themeSlice';
 import { signoutSuccess } from '../redux/user/userSlice';
+import { useEffect, useState } from 'react';
 
 export default function Header() {
   const path = useLocation().pathname;
+  const location = useLocation();
+  const navigate = useNavigate();
   const dispatch = useDispatch();
-  const { currentUser } = useSelector((state) => state.user)
-  const { theme } = useSelector((state) => state.theme)
+  const { currentUser } = useSelector((state) => state.user);
+  const { theme } = useSelector((state) => state.theme);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
 
   const handleSignout = async () => {
     try {
@@ -28,81 +40,87 @@ export default function Header() {
     }
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(location.search);
+    urlParams.set('searchTerm', searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
   return (
     <Navbar className='border-b-2'>
       <Link
         to='/'
         className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'
       >
-        <span className='text-2xl font-bold font-serif tracking-wide text-gray-800'>
-              <span className='relative'>
-                <span className='text-blue-600'>Blog</span>
-                <span className='absolute -bottom-1 left-0 w-full h-1 bg-pink-500 rounded-md'></span>
-              </span>
-              <span className='text-pink-600 ml-1'>Wave</span>
-            </span>
-       
+        <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
+          Sahand's
+        </span>
+        Blog
       </Link>
-      <form className='relative hidden lg:flex items-center flex-grow mx-4'>
-        <input
+      <form onSubmit={handleSubmit}>
+        <TextInput
           type='text'
           placeholder='Search...'
-          className='w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500'
+          rightIcon={AiOutlineSearch}
+          className='hidden lg:inline'
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
         />
-        <button type='submit' className='absolute right-2 top-1/2 transform -translate-y-1/2'>
-          <AiOutlineSearch className='text-gray-400' />
-        </button>
       </form>
       <Button className='w-12 h-10 lg:hidden' color='gray' pill>
         <AiOutlineSearch />
       </Button>
       <div className='flex gap-2 md:order-2'>
-        <Button className='w-12 h-10' color='gray' pill onClick={()=>dispatch(toggleTheme())}>
-          {theme === 'light' ? <FaMoon/> : 
-          <FaSun />}
+        <Button
+          className='w-12 h-10 hidden sm:inline'
+          color='gray'
+          pill
+          onClick={() => dispatch(toggleTheme())}
+        >
+          {theme === 'light' ? <FaSun /> : <FaMoon />}
         </Button>
-        
-        
         {currentUser ? (
-         <Dropdown
-         arrowIcon={false}
-         inline
-         label={
-           <Avatar alt='user' img={currentUser.profilePicture} rounded />
-         }
-       >
-         <Dropdown.Header>
-           <span className='block text-sm'>@{currentUser.username}</span>
-           <span className='block text-sm font-medium truncate'>
-             {currentUser.email}
-           </span>
-         </Dropdown.Header>
-         <Link to={'/dashboard?tab=profile'}>
-           <Dropdown.Item>Profile</Dropdown.Item>
-         </Link>
-         <Dropdown.Divider />
-         <Dropdown.Item onClick={handleSignout}>Sign out</Dropdown.Item>
-       </Dropdown>
-     ) : (
-       <Link to='/sign-in'>
-         <Button gradientDuoTone='purpleToBlue' outline>
-           Sign In
-         </Button>
-       </Link>
-     )}
-     <Navbar.Toggle />
-   </div>
-   <Navbar.Collapse>
-     <Navbar.Link active={path === '/'} as={'div'}>
-       <Link to='/'>Home</Link>
-     </Navbar.Link>
-     <Navbar.Link active={path === '/about'} as={'div'}>
-       <Link to='/about'>About</Link>
-     </Navbar.Link>
-     <Navbar.Link active={path === '/projects'} as={'div'}>
-       <Link to='/projects'>Projects</Link>
-     </Navbar.Link>
-   </Navbar.Collapse>
- </Navbar>
-);
+          <Dropdown
+            arrowIcon={false}
+            inline
+            label={
+              <Avatar alt='user' img={currentUser.profilePicture} rounded />
+            }
+          >
+            <Dropdown.Header>
+              <span className='block text-sm'>@{currentUser.username}</span>
+              <span className='block text-sm font-medium truncate'>
+                {currentUser.email}
+              </span>
+            </Dropdown.Header>
+            <Link to={'/dashboard?tab=profile'}>
+              <Dropdown.Item>Profile</Dropdown.Item>
+            </Link>
+            <Dropdown.Divider />
+            <Dropdown.Item onClick={handleSignout}>Sign out</Dropdown.Item>
+          </Dropdown>
+        ) : (
+          <Link to='/sign-in'>
+            <Button gradientDuoTone='purpleToBlue' outline>
+              Sign In
+            </Button>
+          </Link>
+        )}
+        <Navbar.Toggle />
+      </div>
+      <Navbar.Collapse>
+        <Navbar.Link active={path === '/'} as={'div'}>
+          <Link to='/'>Home</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === '/about'} as={'div'}>
+          <Link to='/about'>About</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === '/projects'} as={'div'}>
+          <Link to='/projects'>Projects</Link>
+        </Navbar.Link>
+      </Navbar.Collapse>
+    </Navbar>
+  );
 }
