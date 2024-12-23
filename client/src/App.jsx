5fdaaf9a6036a0587c0252283d57047f769f4689
TapamitaMondal07 <tapomita.tae007@gmail.com>
2024-12-24 03:33:57 +0530

    add update post functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,6 +12,7 @@ import Footer from './components/Footer.jsx'
 import PrivateRoute from './components/PrivateRoute.jsx'
 import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute';
 import CreatePost from './pages/CreatePost';
+import UpdatePost from './pages/UpdatePost';
 
 import { TextInput } from 'flowbite-react'
 
@@ -30,6 +31,7 @@ function App() {
         </Route>
         <Route element={<OnlyAdminPrivateRoute />}>
           <Route path='/create-post' element={<CreatePost />} />
+          <Route path='/update-post/:postId' element={<UpdatePost />} />
         </Route>
 
         <Route path="/projects" element={<Projects/>}/>
